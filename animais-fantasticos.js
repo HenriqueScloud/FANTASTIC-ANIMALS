4de8e7031d0203558bcb26cdb0586aa733dff210
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-07-16 12:34:38 -0300

    NEW POST - ANIMAIS FANTÁSTICOS
    criação da pagina de script do site animais fantásticos

---
+++ animais-fantasticos.js
@@ -0,0 +1,43 @@
+'use strict'
+
+
+// const animais = document.getElementById('animais')// pega apenas o primero elemento !!!
+// console.log(animais)
+
+// const gridSection = document.getElementsByClassName('grid-section')// pega apenas o primero elemento !!!
+
+// console.log(gridSection)
+
+// const primeiraLi = document.querySelector('li')// pega apenas o primero elemento !!!
+
+// console.log(primeiraLi)
+
+// const linksInternos = document.querySelector('[href^="#faq"')  // pega apenas o primero elemento !!!
+
+// console.log(linksInternos)
+
+
+
+
+// const animaisImg = document.querySelectorAll('.animais img')
+
+// console.log(animaisImg[2])
+
+
+// EXERCICIO 
+
+
+const todasImagens = document.querySelectorAll('img')
+console.log(todasImagens)
+
+
+const linksInternos = document.querySelectorAll('[href^="#"')
+console.log(linksInternos)
+
+
+const primeiroH2 = document.querySelector('.animais-descricao h2')
+console.log(primeiroH2)
+
+const ultimoP = document.querySelectorAll('p')
+console.log(ultimoP[ultimoP.length - 1])
+             
