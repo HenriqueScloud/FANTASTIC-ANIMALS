8e42ac14b581d355bd865bac731e9319d32cff15
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-07-26 11:53:11 -0300

    NEW POST - JAVASCRIPT

--- animais-fantasticos.js
+++ animais-fantasticos.js
@@ -1,43 +1,217 @@
 'use strict'
 
+// const imgs = document.querySelectorAll('img')
 
-// const animais = document.getElementById('animais')// pega apenas o primero elemento !!!
-// console.log(animais)
-
-// const gridSection = document.getElementsByClassName('grid-section')// pega apenas o primero elemento !!!
-
-// console.log(gridSection)
-
-// const primeiraLi = document.querySelector('li')// pega apenas o primero elemento !!!
-
-// console.log(primeiraLi)
-
-// const linksInternos = document.querySelector('[href^="#faq"')  // pega apenas o primero elemento !!!
-
-// console.log(linksInternos)
+// imgs.forEach((item) => {
+//     console.log(item)
+// });
 
 
+// const p = document.querySelectorAll('p')
+
+// p.forEach(element => {
+//     console.log(element.innerText)
+// });
 
 
-// const animaisImg = document.querySelectorAll('.animais img')
-
-// console.log(animaisImg[2])
+// const img = document.querySelector('img').offsetTop
 
 
-// EXERCICIO 
+// console.log(img)
 
 
-const todasImagens = document.querySelectorAll('img')
-console.log(todasImagens)
+
+// const imagens = document.querySelectorAll('img')
+
+// let soma = 0
+// imagens.forEach((item) => {
+    
+//     soma+= item.offsetWidth
+//     console.log(soma)
+    
+
+// })
 
 
-const linksInternos = document.querySelectorAll('[href^="#"')
-console.log(linksInternos)
+
+// const links = document.querySelectorAll('a')
+
+// console.log(links)
 
 
-const primeiroH2 = document.querySelector('.animais-descricao h2')
-console.log(primeiroH2)
+// links.forEach((item) => {
+//     const linkWidth = links.offsetWidth;
+    
 
-const ultimoP = document.querySelectorAll('p')
-console.log(ultimoP[ultimoP.length - 1])
-             
+//     if(linkWidth >= 48 ){
+//         console.log('possui acesssibilidade !')
+//     }else{
+//         console.log('não possui acesssibilidade !')
+//         console.log(item)
+
+//     }
+
+//  })
+
+
+
+
+
+// const img = document.querySelector('img')
+
+// function callback(){
+//    document.body.classList.toggle('azul')
+// }
+
+// /*  crie uma função sempre fora do evento para que seja mais facil de debugar o codigo posteriormente */
+
+
+
+// // img.addEventListener('click', callback)
+
+// function backAzul(event) {
+//     if (event.key === 'f'){
+//         document.body.classList.toggle('azul')
+//     }
+// }
+
+// window.addEventListener('keydown', backAzul)
+
+
+// const imagens = document.querySelectorAll('img')
+
+// imagens.forEach((item)=>{
+//     item.addEventListener('click', callback )
+// })
+
+
+
+// // ----------------- EXERCICIOS --------------------
+
+// const linksInternos = document.querySelectorAll('a[href^="#"')
+
+
+
+// function ativar(event){
+//     event.preventDefault() //impedir comportamento do link
+
+//     linksInternos.forEach((item) => {
+//         item.classList.remove('ativo')
+//     }
+//     )// remover de todo a class ativo 
+
+
+//     event.currentTarget.classList.toggle('ativo') // adicionando a class no item clicado
+// }         
+
+
+
+// linksInternos.forEach((link) => {
+//     link.addEventListener("click",ativar)
+// })
+
+// // ----------------- EXERCICIOS --------------------
+
+
+// const todosElementos = document.querySelectorAll('body *')
+// console.log(todosElementos)
+
+// function elementoClicado(event) {
+    
+//     // event.currentTarget.remove();
+//     console.log(event.currentTarget)
+
+// }
+
+// todosElementos.forEach((elementos) => {
+//     elementos.addEventListener('click', elementoClicado)
+// })
+
+// // ----------------- EXERCICIOS --------------------
+
+
+// function clicouNoT(event){
+//     console.log(event.key);
+//     if(event.key === 't'){
+//         document.documentElement.classList.toggle('textoMaior')
+        
+        
+//     }
+// }
+// window.addEventListener('keydown', clicouNoT)
+
+
+
+
+
+
+
+
+// const mapa = document.querySelector('.mapa')
+// const faq = document.querySelector('.faq')
+
+
+// const h1 = document.querySelector('h1')
+
+// const cloneH1 = h1.cloneNode(true)
+
+
+
+// // ----------------- EXERCICIOS --------------------
+
+
+// const menu = document.querySelector('.menu')
+// const copy = document.querySelector('.copy')
+
+
+// const cloneMenu = menu.cloneNode(true);
+
+// // // copy.appendChild(cloneMenu)
+
+
+
+// // ----------------- EXERCICIOS --------------------
+
+
+// const primeiroDt = document.querySelector('.faq dl dt')
+// console.log(primeiroDt.innerHTML)
+
+// // ----------------- EXERCICIOS --------------------
+
+// const primeiroDD = primeiroDt.nextElementSibling
+// console.log(primeiroDD.remove)
+// // primeiroDD.remove()
+
+
+
+
+
+
+
+
+
+const tabMenu = document.querySelectorAll('.js-tabMenu li')
+const tabContent = document.querySelectorAll('.js-tabContent section')
+console.log(tabContent)
+
+function activeTab(index){
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo')
+    })//removendo a class de todas 
+
+    tabContent[index].classList.add('ativo')// adicionando a class na img clickad
+    
+
+}// criando a função para add a class na img clicada
+
+
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index)
+        
+        
+    })
+})//  add o evento em todas as imgs 
+
+
