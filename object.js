72914b128be02c8a8a27e28921fb393d7458d97c
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-08-23 00:01:45 -0300

    .

--- object.js
+++ object.js
@@ -1,11 +1,26 @@
-function Carro() {
-
-    this.marca = 'marca';
-    this.preço = 2000;
-    
 
 
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+
+    this.andar = function(){
+        console.log(nome + ' Andou');
+    }
 
 }
-const honda =  new Carro();
 
+
+const henrique = new Pessoa('henrique', 19)
+const maia = new Pessoa( 'maia', 25)
+const joao  = new Pessoa('joao', 30);
+
+//  crie uma contrução de função para manipular dom
+
+
+function dom( seletor ) {
+
+    const elementList = document.querySelectorAll(seletor)
+    console.log(elementList)
+
+}
