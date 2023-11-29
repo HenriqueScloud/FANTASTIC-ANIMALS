053ab5737fb32322a6866048941716a36e870d47
Henrique S. Mendes <124588969+HenriqueScloud@users.noreply.github.com>
2023-11-29 18:50:41 -0300

    NEW COMMIT-ANIMAIS FANTASTICOS

--- JS/modules/dropdownMenu.js
+++ JS/modules/dropdownMenu.js
@@ -5,11 +5,9 @@ export default function initDropdownMenu(){
 const dropdownMenus = document.querySelectorAll('[data-dropdown]')
 
 dropdownMenus.forEach(menu => {
-    // menu.addEventListener('touchstart', handleClick);
+    menu.addEventListener('click', handleClick);
 
-    ['touchstart','click'].forEach(userEvent =>{
-        menu.addEventListener(userEvent,handleClick);
-    })
+    
 
 
 })
