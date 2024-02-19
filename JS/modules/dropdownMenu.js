d84d9a502c8d7060157aad4488df36f86ee47443
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2024-02-18 21:38:04 -0300

    NEW COMMIT - PROJETO ANIMAIS FANTASTICOS (DROPODOWNMENU.JS - CORREÇÃO DO BUG DOS MODULOS )

--- JS/modules/dropdownMenu.js
+++ JS/modules/dropdownMenu.js
@@ -1,15 +1,9 @@
-export default function initDropdownMenu(){
+export function initDropdownMenu(){
     const dropdownMenus = document.querySelectorAll('[data-dropdown]')
-
     dropdownMenus.forEach(menu => {
         menu.addEventListener('click', handleClick);
-
-        
-
-
     })
     function handleClick(Event){
         this.classList.toggle('active');
     }
 }
-initDropdownMenu();
