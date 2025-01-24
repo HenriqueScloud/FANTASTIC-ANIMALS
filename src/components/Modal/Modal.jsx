01e64f71f99972ee19099a586d03892fefcf53f2
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-01-24 17:06:51 -0300

    NEW COMMIT - PUSH

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,33 +2,83 @@
 import style from "../Modal/style.module.scss";
 
 
-// eslint-disable-next-line react/prop-types
-export const Modal = ( IsModal) => {
-  console.log(IsModal);
+export const Modal = ({showModal, setshowModal}) => {
   return (
-    <section className={style.modal_container} >
-      <div className={style.modal}>
-        <button
-          className={style.fechar}
-          onClick={() => {
-            IsModal= !IsModal;
-            console.log(IsModal);
-            
-          }}
-        >
-          X
-        </button>
-
-        {IsModal ? ( // Renderiza o formulário apenas se showModal for true
-          <form>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" />
-            <label htmlFor="senha">Senha</label>
-            <input type="password" name="senha" id="senha" autoComplete="off" />
-            <button type="submit">Entrar</button>
-          </form>
-        ) : null }
-      </div>
-    </section>
+    <>
+      <section className={style.modal_container}>
+        {showModal ? (
+          <p>eu sou um </p>
+          // <div className={style.modal}>
+          //   <button
+          //     className={style.fechar}
+          //     onClick={() => {
+          //       console.log("ola mundo");
+          //     }}
+          //   >
+          //     X
+          //   </button>
+          //   <form>
+          //     <label htmlFor="email">E-mail</label>
+          //     <input type="email" id="email" />
+          //     <label htmlFor="senha">Senha</label>
+          //     <input
+          //       type="password"
+          //       name="senha"
+          //       id="senha"
+          //       autoComplete="off"
+          //     />
+          //     <button type="submit">Entrar</button>
+          //   </form>
+          // </div>
+        ) : null}
+      </section>
+    </>
   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log(Modal);
+  
+
+    
+    // <section className={style.modal_container} >
+    // { Modal ? 
+    //   <div className={style.modal}>
+    //     <button
+    //       className={style.fechar}
+    //       onClick={() => {
+    //         console.log('ola mundo');
+            
+    //       }}
+    //     >
+    //       X
+    //     </button>
+    //       <form>
+    //         <label htmlFor="email">E-mail</label>
+    //         <input type="email" id="email" />
+    //         <label htmlFor="senha">Senha</label>
+    //         <input type="password" name="senha" id="senha" autoComplete="off" />
+    //         <button type="submit">Entrar</button>
+    //       </form>
+    //   </div>
+    //   : null }
+    //   </section>
+  
 };
