ec8f37e37f2ae016aee8d068a1bc6d3d714b84fc
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-01-25 17:20:20 -0300

    new commit - push

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import { IoMenuSharp } from "react-icons/io5";
+import { Menu } from "../Menu/Menu";
+
+import "./style.module.scss";
+export const Header = ({ cardModal, setCardModal, cardMenu, setCardMenu }) => {
+  return (
+    <header>
+      <h1 className="titulo">Animais Fantásticos </h1>
+      {cardMenu ? (
+        <Menu setCardMenu={setCardMenu} setCardModal={setCardModal} />
+      ) : (
+        <button>
+          <IoMenuSharp
+            onClick={() => {
+              setCardMenu(true);
+            }}
+          />
+        </button>
+      )}
+    </header>
+  );
+};
