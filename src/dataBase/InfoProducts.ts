a21e58ae90ae0c7f345f00265b0824063040b4aa
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-01-27 18:53:41 -0300

    new commit - push ( typescripts de interfaces e script de database   )

---
+++ src/dataBase/InfoProducts.ts
@@ -0,0 +1,51 @@
+import { IProducts } from "../interfaces/Products.interfaces";
+import { IProducts } from "../interfaces/Products.interfaces";
+
+export const DBInfoProduct: IProducts[] = [
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+
+  {
+    id: 1,
+    name: "Leão",
+    price: 100,
+    description: "Leão é um animal feroz e majestoso",
+    img: "./animais/leao.jpg",
+  },
+];
